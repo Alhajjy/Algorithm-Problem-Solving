be451210621510e49b274e825785ee10cc8cd557
Alhajjy <muhammedhacci2@gmail.comgit>
2025-10-26 23:18:25 +0300

    Main File Added..

---
+++ Level 1/Level_1_MainFile.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+
+#include "Solutions.h"
+using namespace Solutions;
+
+int main() {
+  // =========================
+  //        Solutions
+  // =========================
+
+  /* ========== (1) ========== */
+  // PrintName("Ahmad");
+
+  /* ========== (2) ========== */
+  // PrintNameDynamicly();
+
+  /* ========== (3) ========== */
+  // PrintNumber(isEvenOdd(ReadNumber()));
+
+  /* ========== (4) ========== */
+  // showResult(isAccepted(ReadInfo()));
+
+  /* ========== (5) ========== */
+  // showResult2(isAccepted2(ReadInfo2()));
+
+  /* ========== (6) ========== */
+  // reversed:
+  // PrintFullName(getFullName(ReadFullName(), enFullName::Reversed));
+  // normal:
+  // PrintFullName(getFullName(ReadFullName(), enFullName::Normal));
+
+  /* ========== (7) ========== */
+   //int number;
+   //ReadNumberByReferans(number);
+   //PrintNum(calcHalfNum(number), number);
+
+  /* ========== (8) ========== */
+  // PrintResultMark(checkMark(ReadMark()));
+
+  /* ========== (9) ========== */
+  // PrintResultOfSumNumbers(SumNums(ReadNums()));
+
+  /* ========== (10) ========== */
+  // PrintAvgResult(AverageNums(ReadNums()));
+
+  /* ========== (11) ========== */
+  // PrintResultAvg(isPassed(AverageNums(ReadNums())));
+
+  /* ========== (12) ========== */
+  // PrintResultMaxOf2Nums(getMax(Read2Nums()));
+
+  /* ========== (13) ========== */
+  // PrintResultMax3Nums(getMaxOf3Nums(Read3Nums()));
+
+  /* ========== (14) ========== */
+  // PrintResultOfSwap2Nums(SwapTwoNums(Read2Nums()));
+
+  /* ========== (15) ========== */
+  // PrintResultOfRectangleArea(RectangleArea(Read2Nums()));
+
+  /* ========== (16) ========== */
+  // PrintResultOfRectangleArea2(RectangleArea2(Read2Nums()));
+
+  /* ========== (17) ========== */
+  // PrintResultOfTriangleArea(triangleArea(Read2Nums()));
+
+  /* ========== (18) ========== */
+  // PrintResultOfCircleArea(CircleArea(ReadNumber()));
+
+  /* ========== (19) ========== */
+  // PrintResultOfCircleArea2(CircleArea2(ReadNumber()));
+
+  /* ========== (20) ========== */
+  // PrintResultCircleArea3(CircleArea3(ReadNumber()));
+
+  /* ========== (21) ========== */
+  // PrintResultCircleArea4(CircleArea4(ReadNumber()));
+
+  /* ========== (22) ========== */
+  // PrintResultOfCircleArea5(CircleArea5(Read2Nums()));
+
+  /* ========== (23) ========== */
+  // PrintResultOfCircleArea6(CircleArea6(Read3Nums()));
+
+  /* ========== (24) ========== */
+  // PrintResultOfValidationRange(ReadAge());
+
+  /* ========== (25) ========== */
+  // PrintResultOf_ReadUntilAgeBetween(ReadUntilAgeBetween());
+
+  /* ========== (26) ========== */
+  // PrintSum(calcSumToN(ReadNumber()));
+
+  /* ========== (27) ========== */
+  // PrintFromNToZero(ReadNumber());
+
+  /* ========== (28) ========== */
+  // PrintSumOddNums(calcSumOddToN(ReadNumber()));
+
+  /* ========== (29) ========== */
+  // PrintSumOf_calcSumEvenToN(calcSumEvenToN(ReadNumber()));
+
+  /* ========== (30) ========== */
+  // Factoial(ReadPozitifNumber("Enter Pozitif Number!\n"));
+
+  /* ========== (31) ========== */
+  // Power2_3_4(ReadNumber());
+
+  /* ========== (32) ========== */
+  // calcNumPow(ReadNumPow());
+
+  /* ========== (33) ========== */
+  // char letter = GetGradeLetter(ReadNumberInRange(0, 100));
+  // cout << letter;
+
+  /* ========== (34) ========== */
+  // float TotalSales = ReadTotalSales();
+  // cout << "Commission Percentage = " << whichPercentage(TotalSales) << endl;
+  // cout << "Total Commission = " << calcSalesPercntg(TotalSales) << endl;
+
+  /* ========== (35) ========== */
+  // PrintResultOf_calcPennies();
+
+  /* ========== (36) ========== */
+  // PrintOperationResult(CheckCalcOpr(ReadDetails()));
+
+  /* ========== (37) ========== */
+  // cout << "Result is: " << EnterSumNums();
+
+  /* ========== (38) ========== */
+  // PrintResultOf_CheckPrime(CheckPrime(ReadPozitifNumber("Enter num to see
+  // Prime or Not Prime!\n")));
+
+  /* ========== (39) ========== */
+  // float TotalBill = ReadPozitifNumber("Enter The Total Bill!\n");
+  // float CashPaid = ReadPozitifNumber("Enter The Cash Paid!\n");
+  // PrintResultOf_Calc(Calc(TotalBill, CashPaid));
+
+  /* ========== (40) ========== */
+  // float BillValue = ReadPozitifNumber("Enter The Bill Value!\n");
+  // PrintResult(Calc2(BillValue));
+
+  /* ========== (41) ========== */
+  // float Hours = ReadPozitifNumber("Enter Hours Number!\n");
+  // float Days = CalcDaysNum(Hours);
+  // float Weeks = CalcWeeksNum(Hours);
+  // PrintResultOfCalculation(Hours, Days, Weeks);
+
+  /* ========== (42) ========== */
+  // int Seconds = CalcSeconds(ReadDuration());
+  // cout << "Total Seconds is: " << Seconds;
+
+  /* ========== (43) ========== */
+  // PrintResultOf_CalcDetails(CalcDetails(ReadPozitifNumber("Enter Seconds
+  // number?\n")));
+
+  /* ========== (44) ========== */
+  // cout << WhichDay(ReadPozitifNumber("Enter num (1-7)\n"));
+
+  /* ========== (45) ========== */
+  // cout << WhichMonth(ReadMonth());
+
+  /* ========== (46) ========== */
+  // PrintLetterAtoZ();
+
+  /* ========== (47) ========== */
+  // int Loan = ReadPozitifNumber("Enter Loan\n");
+  // int MonthlyPayment = ReadPozitifNumber("Enter Monthly Payment\n");
+  // cout << HowMonth(Loan, MonthlyPayment) << " Months";
+
+  /* ========== (48) ========== */
+  // int Loan = ReadPozitifNumber("Enter Loan\n");
+  // int Months = ReadPozitifNumber("Enter Months\n");
+  // cout << MonthlyPayment(Loan, Months) << " $ in each month.";
+
+  /* ========== (49) ========== */
+  //if (CheckPin(ReadPin("Enter Pin\n"))) {
+  //  system("color 2F");
+  //  cout << "Your Balance is: " << 7500 << '\n';
+  //};
+
+  /* ========== (50) ========== */
+   //if (Login()) {
+   //  system("color 2F");
+   //  cout << "Your Balance is: " << 7500 << '\n';
+   //};
+
+  system("pause>0");
+}
